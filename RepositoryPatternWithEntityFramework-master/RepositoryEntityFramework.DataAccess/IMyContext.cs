bcc8e2f23df34ce7332389112f600d906c36d391
Joel Ariel Sosa <joelarielsosa@gmail.com>
2016-07-16 12:00:37 -0400

    Implementando Patron Repositorio JASOSA

---
+++ RepositoryPatternWithEntityFramework-master/RepositoryEntityFramework.DataAccess/IMyContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using RepositoryEntityFramework.DataAccess.Model;
+
+namespace RepositoryEntityFramework.DataAccess
+{
+    public interface IMyContext
+    {
+        DbSet<User> User { get; set; }
+    }
+}
